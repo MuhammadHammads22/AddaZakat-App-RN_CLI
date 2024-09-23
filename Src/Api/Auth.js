2598ed85f9ab393097d550ddacfc36433dc917db
MuhammadHammads22 <110112864+MuhammadHammads22@users.noreply.github.com>
2024-09-24 02:26:18 +0500

    Update Auth.js
    405 error solution

--- Src/Api/Auth.js
+++ Src/Api/Auth.js
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 // Define a service using a base URL and expected endpoints
 export const Api = createApi({
   reducerPath: 'userAuthApi',
-  baseQuery: fetchBaseQuery({ baseUrl:'http://api.addazakat.com/user/' }),
+  baseQuery: fetchBaseQuery({ baseUrl:'https://api.addazakat.com/user/' }),
   endpoints: (builder) => ({
     login: builder.mutation({
       query: (body) => ({
